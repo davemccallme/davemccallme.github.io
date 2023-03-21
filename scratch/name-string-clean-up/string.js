8db7e03f1bd7160e57eb6fe09f54cc7ef1e799e8
Dave <david.mccall2@pge.com>
2023-03-20 21:47:44 -0700

    Transaction map uplaod

---
+++ scratch/name-string-clean-up/string.js
@@ -0,0 +1,22 @@
+/*turn string into an array*/
+const longString = "string1 string2";
+const arrList = longString.split(" ");
+console.log(arrList);
+/*pass array into function loop*/
+
+for (let i=0; i< arr.length-1; i++) {
+    let arr = arrList;
+    function validate(arr[i]) {
+        var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+        var name = document.getElementById('nameInput').value;
+        if(!regName.test(name)){
+            alert('Invalid name given.');
+        }else{
+            alert('Valid name given.');
+        }
+
+        arr.
+    
+    } console.log(arr[i]);
+    
+}
